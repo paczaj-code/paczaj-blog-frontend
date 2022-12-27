543086dfecb983307ca1c78769128c486823cd13
paczaj-code <marcinpaczkowski@icloud.com>
2022-12-27 20:23:24 +0100

    Change structure  - to correct

--- components/Header/Logo/Logo.tsx
+++ components/Header/Logo/Logo.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import eyes from '../../../public/images/pngwing.com(5).png';
+import Image from 'next/image';
 
 const Logo = () => {
-  return <h3 className="logo">Paczaj.WTF</h3>;
+  return (
+    <div className="logo__wrapper">
+      <Image src={eyes} alt="eyes" height={37} />
+      <h1 className="logo">
+        P<span className="logo_a">a</span>CZ<span className="logo_a">a</span>J
+        {/* <span className="logo_domain">wtf</span> */}
+      </h1>
+    </div>
+  );
 };
 
 export default Logo;
