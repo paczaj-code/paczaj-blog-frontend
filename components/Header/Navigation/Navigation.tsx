97769e8c4917c7b221dd5c2d81c29e94762fe528
paczaj-code <marcinpaczkowski@icloud.com>
2022-12-27 20:23:47 +0100

    Refactor

--- components/Header/Navigation/Navigation.tsx
+++ components/Header/Navigation/Navigation.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import SubMenu from './SubMenu'
-interface MenuItem {
+import SubMenu from './SubMenu';
+import Heading from '@/components/UI/Heading/Heading';
+import Icon from '@/components/Icon/Icon';
+
+export interface MenuItemTypes {
   name: string;
-  target: string;
+  target?: string;
+  icon?: string;
 }
 
-interface MenuTypes extends MenuItem {
-  subMenu?: { name: string; subMenuItems: MenuItem[] }[];
+export interface MenuTypes extends MenuItemTypes {
+  subMenu?: { name: string; icon?: string; subMenuItems: MenuItemTypes[] }[];
 }
 
 const Navigation = ({
@@ -20,13 +24,23 @@ const Navigation = ({
   showMenu: boolean;
   onClick: React.MouseEventHandler<HTMLElement>;
 }) => {
+  const [showSubMenu, setShowSubmenu] = useState<string | undefined>(undefined);
   const router = useRouter();
+
   return (
     <nav
       id="main__menu"
       className={showMenu ? 'show__menu' : ''}
       onClick={onClick}
     >
+      <div className="mobile-menu__header">
+        <Heading
+          headingLevel="h2"
+          heading_label="Menu"
+          heading_prefix="mobile_title"
+        />
+        <Icon icon="icon-circle-left" icon_modifier="close-menu" />
+      </div>
       <ul className="menu__list">
         <li className="menu__item">
           <Link
@@ -41,15 +55,15 @@ const Navigation = ({
         {menuItems.map((menuItem, index) => {
           const matcher = new RegExp(menuItem.target + '/', 'g');
           return (
-            <li className="menu__item" key={index}>
-              {Boolean(menuItem.subMenu) ? (
-                <>
-                <p>{menuItem.name}</p>
-                <SubMenu subMenu={menuItem.subMenu} />
-                </>
-              ) : (
+            <li
+              className="menu__item"
+              key={index}
+              onMouseOver={() => setShowSubmenu(menuItem.name)}
+              onMouseLeave={() => setShowSubmenu(undefined)}
+            >
+              <>
                 <Link
-                  href={menuItem.target}
+                  href={menuItem.target!}
                   className={
                     router.asPath === menuItem.target ||
                     matcher.test(router.asPath)
@@ -59,7 +73,16 @@ const Navigation = ({
                 >
                   {menuItem.name}
                 </Link>
-              )}
+                {menuItem.subMenu?.length && (
+                  <SubMenu
+                    subMenu={menuItem.subMenu}
+                    prev_path={menuItem.target}
+                    showSubmenu={showSubMenu}
+                    subMenuName={menuItem.name}
+                    setShowSubmenu={setShowSubmenu}
+                  />
+                )}
+              </>
             </li>
           );
         })}
