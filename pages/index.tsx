793e0d7bfb2f0a0a1b226f1e50b939dbd841aec9
paczaj-code <marcinpaczkowski@icloud.com>
2022-12-28 19:52:48 +0100

    Style chnage

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,89 @@ export default function Home() {
       </Head>
 
       <main>
-        <h2>Welcome</h2>
+        <h1>Aut consequatur optio id ullam voluptatem et maiores ducimus. </h1>
+        <p>
+          Lorem ipsum dolor sit amet. Est sint quia est dolores repellat in
+          debitis architecto aut dolorem deserunt et perspiciatis praesentium
+          qui aliquam minima? Ut enim laudantium qui soluta maiores aut omnis
+          assumenda ea maxime rerum. Et repudiandae libero ut voluptas officiis
+          eos quisquam deserunt rem voluptatem magni.{' '}
+        </p>
+        <h2>Rem culpa nulla qui dolore inventore. </h2>
+        <p>
+          Et quisquam inventore sit provident numquam et expedita
+          exercitationem. Sed beatae harum qui magni laboriosam sed voluptate
+          quidem eos blanditiis aliquam non facilis aliquam. Sit voluptatum
+          alias ut Quis assumenda non temporibus expedita id aliquid rerum. Et
+          temporibus aperiam quo esse totam et placeat voluptas qui libero
+          dolores.{' '}
+        </p>
+        <h3>Sit similique consequatur et fugiat quia sit quidem velit. </h3>
+        <p>
+          Non repudiandae vero qui ratione pariatur eum officiis necessitatibus
+          ea quasi nostrum. Non iste quia non rerum fuga cum possimus alias et
+          necessitatibus voluptatem ea iusto molestiae et nihil quam est quasi
+          quae.{' '}
+        </p>
+        <h4>
+          Aut nobis repellendus non deleniti pariatur ab quia voluptatibus.{' '}
+        </h4>
+        <p>
+          Ea minus sequi et porro repellat hic rerum autem ut necessitatibus
+          necessitatibus 33 dolor reprehenderit et deserunt nemo hic adipisci
+          praesentium. Qui expedita iure id quam dicta ut quod assumenda!{' '}
+        </p>
+        <h5>
+          Ut praesentium dignissimos sit nulla internos ea necessitatibus
+          consequatur.{' '}
+        </h5>
+        <p>
+          Qui voluptatum veniam ea omnis pariatur non perspiciatis consequatur.
+          Qui dolorem rerum ea eius possimus qui enim voluptatem ea velit
+          tempora. Id corrupti illum sit accusamus ducimus eum illo repellat eum
+          laudantium minus et nesciunt voluptates qui eveniet facilis. Nam optio
+          veniam et incidunt vitae sit earum ullam sed numquam omnis eum nemo
+          temporibus non sint omnis aut velit laboriosam.{' '}
+        </p>
+        <p>
+          Ut consectetur quas 33 commodi sapiente aut fuga repellendus qui
+          voluptate eveniet. Sed commodi earum est modi dolore quo praesentium
+          quidem ad accusamus corrupti? Et asperiores consequatur est rerum
+          saepe qui praesentium quae est voluptas perspiciatis est culpa galisum
+          est odio aliquid. In assumenda blanditiis eum pariatur rerum aut
+          repellat sapiente!{' '}
+        </p>
+        <p>
+          Est consectetur magnam ut odit adipisci qui tenetur voluptatum qui
+          ullam ipsum? Eos rerum quibusdam non laudantium dolores ea nulla dicta
+          et quam explicabo ab quia sapiente est doloribus odio.{' '}
+        </p>
+        <p>
+          Ea natus iusto aut repellat modi vel voluptatem minima. Ea omnis
+          dolorem qui explicabo ducimus aut dolores expedita eos rerum voluptas.
+          Et magnam repudiandae qui eaque voluptatem non quaerat repellendus.{' '}
+        </p>
+        <p>
+          Ut placeat doloremque in temporibus quasi qui dolorem modi est
+          molestiae quisquam et dolor omnis. Ut dignissimos eligendi a
+          voluptatem internos ex autem porro et itaque excepturi. Eum expedita
+          voluptatem et enim ipsam et aperiam molestiae sed quia eligendi? Est
+          obcaecati obcaecati sit itaque nesciunt non consectetur repellendus
+          sit animi doloribus est error animi qui voluptates nostrum.{' '}
+        </p>
+        <p>
+          Quo ipsa rerum hic dolores maxime qui odit eligendi qui voluptatem
+          repellendus sit quia eaque. Et doloribus voluptate est deleniti
+          galisum qui iste esse quo quia reprehenderit et repellat quam id atque
+          sint.{' '}
+        </p>
+        <p>
+          Quo galisum nesciunt in velit laborum et reiciendis minima id repellat
+          pariatur sit enim sint est ullam autem. Hic Quis quia a dolores
+          voluptates est placeat eius id illo voluptas? Qui eaque velit eum
+          officia laborum et delectus nisi aut neque doloremque eos quasi ipsam
+          quo autem alias est quae veniam.{' '}
+        </p>
       </main>
     </div>
   );
